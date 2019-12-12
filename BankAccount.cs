fffa9809ddb5c7270725f212c1ecf226b3f08d18
davobaghoyan <57056544+davobaghoyan@users.noreply.github.com>
2019-12-12 23:06:38 +0400

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        public string name { get; set; }
+        public double balance { get; set; }
+        public double interestrate { get; set; }
+        BankAccount () { }
+          public   BankAccount(string nameofcustomer, double customerBalance, double customerInterestRate)
+        {
+            name = nameofcustomer;
+            balance = customerBalance;
+            if (customerInterestRate >= 0.0 && customerInterestRate <= 22.0)
+                interestrate = customerInterestRate;
+            else
+                throw new WrongRateInteretException("Interestrate must be between 0:22");
+        }
+
+        public void Withdraw (double requestedSum)
+        {
+            if (requestedSum <= this.balance)
+                this.balance -= requestedSum;
+            else
+                throw new NotSufficentAmountException(" You don't have sufficent amount for withdraw.");
+        }
+
+        public void Deposit (double depositSum)
+        {
+            this.balance += depositSum;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} has {balance}$. His interestrate is {interestrate}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                BankAccount customer1 = new BankAccount("John" ,500, 21);
+            
+            try
+            {
+                customer1.Withdraw(2000);
+            }
+
+            catch (NotSufficentAmountException ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(customer1);
+            Console.Read();
+        }
+    }
+}
